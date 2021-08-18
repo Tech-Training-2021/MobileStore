72db93a8a94b74d0f874aa23f7c5f497ebc3b1fa
samuel <samueljoy1999@gmail.com>
2021-08-18 21:18:31 +0530

    initial commit

---
+++ MobileStore/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MobileStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SignUp();
+        }
+
+
+        public static void SignUp()
+        {
+
+            Console.WriteLine("<----- SignUp Page ----->\n");
+
+            Authentication.AuthenticateID();
+
+            Authentication.AuthenticateUsername();  
+         
+            Authentication.AuthenticatePassword();
+
+            Console.WriteLine("\nEnter FirstName: ");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("\nEnter LastName: ");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("\nEnter Location: ");
+            string location = Console.ReadLine();
+
+            Authentication.AuthenticateRest(firstname, lastname, location);
+
+            Authentication.AuthenticateMobile();
+
+            Authentication.AuthenticateDOB();
+
+            Authentication.Register();
+
+        }
+    }
+}
