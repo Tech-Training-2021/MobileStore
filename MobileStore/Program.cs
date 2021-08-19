18c60c6a3abd534e35f93aa5cc84beff07ac6572
samuel <samueljoy1999@gmail.com>
2021-08-19 16:35:18 +0530

    Implemented Abstraction and Encapsulation

--- MobileStore/Program.cs
+++ MobileStore/Program.cs
@@ -8,38 +8,7 @@ namespace MobileStore
     {
         static void Main(string[] args)
         {
-            SignUp();
-        }
-
-
-        public static void SignUp()
-        {
-
-            Console.WriteLine("<----- SignUp Page ----->\n");
-
-            Authentication.AuthenticateID();
-
-            Authentication.AuthenticateUsername();  
-         
-            Authentication.AuthenticatePassword();
-
-            Console.WriteLine("\nEnter FirstName: ");
-            string firstname = Console.ReadLine();
-
-            Console.WriteLine("\nEnter LastName: ");
-            string lastname = Console.ReadLine();
-
-            Console.WriteLine("\nEnter Location: ");
-            string location = Console.ReadLine();
-
-            Authentication.AuthenticateRest(firstname, lastname, location);
-
-            Authentication.AuthenticateMobile();
-
-            Authentication.AuthenticateDOB();
-
-            Authentication.Register();
-
+            Authentication.SignUp();
         }
     }
 }
